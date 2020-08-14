3248e7a2413aa8332156ec6633abfbe99f50f010
hubisan <hubisan@gmail.com>
2020-08-15 00:01:12 +0200

    update

---
+++ {{{pgk-name}}}.el
@@ -0,0 +1,60 @@
+;;; {{{pkg-name}}}.el --- TODO description -*- lexical-binding: t -*-
+
+;; TODO Copyright (C) {{{year}}} Daniel Hubmann <hubisan@gmail.com>
+
+;; TODO add author, maintaner and url.
+;; Author: Daniel Hubmann <hubisan@gmail.com>
+;; Maintainer: Daniel Hubmann <hubisan@gmail.com>
+;; URL: https://github.com/{{{owner}}}/{{{repo}}}
+;; TODO add keywords, call function `finder-by-keyword' to get a list of allowed.
+;; Keywords:
+;; Version: 0.1.0
+;; TODO add requires.
+;; Package-Requires: ((emacs "27.1"))
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; TODO add description.
+
+;;; Code:
+
+;; * Requirements
+
+;; * Customization
+
+;; TODO add customization if needed.
+;; (defgroup {{{pkg-name}}} nil
+;;   "TODO"
+;;   :group 'convenience)
+
+;; TODO add code here with whatever headings needed.
+;; Using `outshine-mode', which automatically detects outline used for headings.
+
+;; * Helpers
+
+;; * <Topic Heading>
+
+;; * <Topic Heading>
+
+;; * ...
+
+;; * Footer
+
+(provide '{{{pkg-name}}})
+
+;;; {{{pkg-name}}}.el ends here
